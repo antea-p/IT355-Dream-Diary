456e62fedc1b995257ed901d0102ca0c2f47891e
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-04-30 17:36:32 +0200

    initial commit

---
+++ src/main/java/ac/rs/metropolitan/anteaprimorac5157/It355PzAnteaPrimorac5157Application.java
@@ -0,0 +1,13 @@
+package ac.rs.metropolitan.anteaprimorac5157;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355PzAnteaPrimorac5157Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It355PzAnteaPrimorac5157Application.class, args);
+	}
+
+}
