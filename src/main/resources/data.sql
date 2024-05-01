3d4fdf61a8d6afb2971966b2711a37b1dcae10c9
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-05-01 19:26:38 +0200

    inserted test users into data.sql and created DiaryController

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,7 @@ CREATE TABLE dream_diary.users
 );
 
 -- TODO: Insert other tables and entries, later
+
+INSERT INTO dream_diary.users (user_id, username, password, is_admin)
+VALUES (1, 'omori', '$2a$12$hJqGL7dZIgEecr8lamjVkOM6fKSvUFWAup8KZhHD0X5ppIzNcsqXK', false),
+       (2, 'sunny', '$2a$12$hJqGL7dZIgEecr8lamjVkOM6fKSvUFWAup8KZhHD0X5ppIzNcsqXK', true)
