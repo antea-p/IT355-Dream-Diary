ad9c04d6c708fa4496592c0c9ef45f09d9ac6803
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-05-08 23:48:44 +0200

    - renamed diary.html to list.html - added a couple of methods to DiaryController

--- src/main/java/ac/rs/metropolitan/anteaprimorac5157/controller/DiaryController.java
+++ src/main/java/ac/rs/metropolitan/anteaprimorac5157/controller/DiaryController.java
@@ -1,15 +1,59 @@
 package ac.rs.metropolitan.anteaprimorac5157.controller;
 
+import ac.rs.metropolitan.anteaprimorac5157.entity.DiaryEntry;
+import ac.rs.metropolitan.anteaprimorac5157.service.DiaryEntryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/diary")
 public class DiaryController {
 
+    private final DiaryEntryService diaryEntryService;
+
+    @Autowired
+    public DiaryController(DiaryEntryService diaryEntryService) {
+        this.diaryEntryService = diaryEntryService;
+    }
+
+    // TODO: search & sort
     @GetMapping
-    public String showDiary() {
-        return "diary";
+    public String showDiaryList(Model model) {
+        model.addAttribute("diaryEntries", diaryEntryService.list());
+        return "list";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("diaryEntry", new DiaryEntry());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String saveDiaryEntry(@ModelAttribute DiaryEntry diaryEntry, BindingResult result) {
+        if (result.hasErrors()) {
+            // Provjera vrijednosti ID-a kako bi se utvrdilo je li korisnik kreirao ili ažurirao diaryEntry
+            if (diaryEntry.getId() == null) {
+                return "create";
+            } else {
+                return "edit";
+            }
+        }
+        diaryEntryService.save(diaryEntry);
+        return "redirect:/diary";
+    }
+
+    // TODO: edit i delete
+
+
+    @GetMapping("/show/{id}")
+    public String showDiaryEntry(@PathVariable("id") Integer id, Model model) {
+        DiaryEntry diaryEntry = diaryEntryService.get(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid diaryEntry Id: " + id));
+        model.addAttribute("diaryEntry", diaryEntry);
+        return "show";
     }
 }
